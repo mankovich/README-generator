bfecdd5ebf968f06595ae358d31780c1b805f718
Joey M <172998670+mankovich@users.noreply.github.com>
2024-07-25 10:00:30 -0400

    updates to code, including inquirer questions and markdown generator

--- index.js
+++ index.js
@@ -1,13 +1,91 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const colors = require('colors');
+const readmeGen = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'Please supply your GitHub username:',
+    'and email address:',
+    'What is the title of your project?',
+    'Please briefly describe your project:',
+    'Installation steps?',
+    'Usage instructions?',
+    'License, if any:',
+    'Contributing guidelines?',
+    'Tests?'
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const licenses = [
+    'MIT',
+    'BSD 3-Clause',
+    'GNU GPL v3',
+    'No license'
+]
+
+// writing data to README.md ... parameter: data: a string containing all content from generateMarkdown.js
+function writeToFile(data) {
+    fs.writeFile('./md/README.md', readmeGen.generateMarkdown(data), (err) =>
+    err ? console.log(err) : console.log(color.neonGreen('\nFind the generated README in md folder')))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function getUserInfo() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: colors.bgBrightYellow(questions[0]),
+            name: 'username'
+        },
+        {
+            type: 'input',
+            message: colors.bgBrightYellow(questions[1]),
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: colors.bgBrightYellow(questions[2]),
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: colors.bgBrightYellow(questions[3]),
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: colors.bgBrightYellow(questions[4]),
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: colors.bgBrightYellow(questions[5]),
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: colors.bgBrightYellow(questions[6]),
+            name: 'license'
+        },
+        {
+            type: 'input',
+            message: colors.bgBrightYellow(questions[7]),
+            name: 'contributing'
+        },
+        {
+            type: 'input',
+            message: colors.bgBrightYellow(questions[8]),
+            name: 'tests'
+        }
+    ])
+    .then(data => {
+        generateMarkdown(data)
+    })
+}
 
 // Function call to initialize app
-init();
+getUserInfo();
+
+module.exports = {
+    getUserInfo
+}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,68 @@
+const data = require('.\README-generator\index.js')
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
+
+function renderTitle(data) {
+  const title = data.title
+
+  return `# ${title}`;
+}
+
+function renderSection(sectionTitle, content) {
+  return `\n\n##${sectionTitle}\n${content}`;
+}
+
+function renderQuestionsSection(username, email) {
+  const {username, email} = data.email
+
+  return `\n\n##Questions\n` + 
+  `My GitHub username is ${username}, and you may access my GitHub by clicking this [link.](https://www.github.com/${username})\n` +
+  `If you have any questions, please send them to me at ${email}.`
+}
+
+function renderTOC(title) {
+  return '\n\n##Table of Contents' + '\n-- ' + title +
+  '\n - [Description](#Description)' + 
+  '\n - [Installation](#Installation)' +
+  '\n - [Usage](#Usage)' +
+  '\n - [Contributing](#Contributing)' +
+  '\n - [Tests](#Tests)' +
+  '\n - [License](#License)' +
+  '\n - [Questions](#Questions)';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  const {username, email, title, description, installation, usage, license, contributing, tests} = data
+  
+  renderTitle(title) +
+  renderTOC(title) +
+  renderSection('Description', description) +
+  renderSection('Installation', installation) +
+  renderSection('Usage', usage) +
+  renderSection('Contributing', contributing) +
+  renderSection('Tests', tests) +
+  renderLicenseSection(license) +
+  renderQuestionsSection(username, email);
+;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+  generateMarkdown
+};
